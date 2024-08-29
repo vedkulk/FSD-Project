0d7bd7fc2fd425d633645a62cf53e7f9c820cb3d
vedkulk <kulkarnived3@gmail.com>
2024-08-29 23:54:37 +0530

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 const App = () => {
   return (
-    <div><Button>Click me</Button></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Navigate to='/auth'/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
