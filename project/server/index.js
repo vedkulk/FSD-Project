a1bbc96ac094ea2e474bdc1bc41d21ced15aa96c
vedkulk <kulkarnived3@gmail.com>
2024-09-09 19:43:08 +0530

    auth controllers

--- project/server/index.js
+++ project/server/index.js
@@ -14,10 +14,12 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
-
 const server = app.listen(port, ()=>{
     console.log(`Server is running on https://localhost:${port}`)
 })
 mongoose.connect(databaseURL)
 .then(()=>console.log('Database Connection successful'))
 .catch(err=>console.log(err.message))
+// Add this after connecting to MongoDB
+const chatSchema = new mongoose.Schema({ message: String });
+const Chat = mongoose.model("Chat", chatSchema);
---
+++ project/server/models/UserModel.js
@@ -0,0 +1,41 @@
+import { genSalt } from "bcrypt"
+import mongoose from "mongoose"
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique:true
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"]
+    },
+    firstName:{
+        type: String,
+        required: false
+    },
+    lastName:{
+        type: String,
+        required: false
+    },
+    image:{
+        type: String,
+        required: false
+    },
+    colour:{
+        type: Number,
+        required: false
+    }, 
+    profielSetup:{
+        type:Boolean,
+        default:false
+    }
+})
+userSchema.pre("save", async function(next){
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt)
+    next();
+})
+
+const User = mongoose.model("Users", userSchema)
+export default User;
