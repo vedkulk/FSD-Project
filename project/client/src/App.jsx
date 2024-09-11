1fe7c5f9bbbcb088764c5449c7f81d5a080e7a37
vedkulk <kulkarnived3@gmail.com>
2024-09-11 18:43:09 +0530

    private and auth routes function

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -3,15 +3,48 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Auth from './pages/auth'
 import Chat from './pages/chat'
 import Profile from './pages/profile'
+import { useAppStore } from './store'
 
+const PrivateRoute = ({children}) => {
+  const {userInfo} = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to='/auth' />
+}
+
+const AuthRoute = ({children}) => {
+  const {userInfo} = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to='/chat' />:children 
+}
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route 
+          path='/auth' 
+          element={
+          <AuthRoute>
+            <Auth/>
+          </AuthRoute>
+          }
+        />
+        <Route 
+          path='/chat' 
+          element={
+          <PrivateRoute>
+            <Chat/>
+          </PrivateRoute>
+          }
+        />
+        <Route 
+          path='/profile' 
+          element={
+          <PrivateRoute>
+            <Profile/>
+          </PrivateRoute>
+          }
+        />
         <Route path='*' element={<Navigate to='/auth'/>}/>
-        <Route path='/auth' element={<Auth/>}/>
-        <Route path='/chat' element={<Chat/>}/>
-        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   )
