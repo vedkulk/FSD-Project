35dfd6b2ca2f9dfd4dab3130069e64242bb71dab
vedkulk <kulkarnived3@gmail.com>
2024-10-19 22:45:33 +0530

    empty chat UI

--- project/client/src/pages/chat/components/empty-chat-container copy/index.jsx
+++ project/client/src/pages/chat/components/empty-chat-container copy/index.jsx
@@ -2,13 +2,18 @@ import { animationDefaultOptions } from '@/lib/utils'
 import Lottie from 'react-lottie'
 const EmptyChatContainer = () => {
   return (
-    <div className="flex-1 md:bg-[#1c1d25] md: flex flex-col justofy-center items-center hidden duration-1000 transition-all">
-        <Lottie
-            isClickedToPauseDisabled={true}
-            height={200}
-            width={200}
-            options={animationDefaultOptions}
-        />
+    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">
+      <Lottie
+        isClickToPauseDisabled={true}
+        height={200}
+        width={200}
+        options={animationDefaultOptions}
+      />
+        <div className='text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center'>
+          <h3 className='poppins-medium'>
+            Hi! Welcome to chat<span className='text-red-500'>MIT</span>
+          </h3>
+        </div>
     </div>
   )
 }
